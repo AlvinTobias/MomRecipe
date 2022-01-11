02ceabc30e0497916dccd3fe81d953f8c1c05257
Alvin <alvintob@gmail.com>
2022-01-11 16:30:49 -0500

    Change Item to ScorllView and LazyVStack

--- MomRecipe/View/MomRecipeList.swift
+++ MomRecipe/View/MomRecipeList.swift
@@ -8,47 +8,59 @@
 import SwiftUI
 
 struct MomRecipeList: View {
-   // @ObservedObject var RecipeModel:MomRecipeModel = MomRecipeModel()
+    // @ObservedObject var RecipeModel:MomRecipeModel = MomRecipeModel()
     @EnvironmentObject var RecipeModel:MomRecipeModel
     
     var body: some View {
         VStack
         {
-           
+            Text("Mom's Recipe").font(.title).bold().foregroundColor(.cyan).shadow(radius: 10)
             NavigationView
             {
-            
-             
-                    List(RecipeModel.MomRecipeList) {A in
-                        NavigationLink {
-                           RecipeDetail(recipeDetail: A)
-                        } label: {
-                            HStack
-                            {
-                                Image(A.image).resizable().frame(width: 100, height: 100, alignment: .leading).clipped().cornerRadius(20).scaledToFill()
-                                VStack(alignment: .leading)
+                ScrollView
+                {
+                    
+                    LazyVStack(alignment : .leading)
+                    {
+                        ForEach (RecipeModel.MomRecipeList) { A in
+                            //List(RecipeModel.MomRecipeList) {A in
+                            NavigationLink {
+                                RecipeDetail(recipeDetail: A)
+                            } label: {
+                                HStack
                                 {
-                                    Spacer()
-                                    Text(A.name).font(.title3)
-                                    Spacer()
-                                    HStack
+                                    Image(A.image).resizable().frame(width: 100, height: 100, alignment: .leading).clipped().cornerRadius(20).scaledToFill()
+                                    VStack(alignment: .leading)
                                     {
-                                    Text("Servings:" + String(A.servings)).font(.subheadline)
-                                    Text("Time:" + String(A.totalTime)).font(.subheadline)
-                                       
+                                        Spacer()
+                                        Text(A.name).font(.title3)
+                                        Spacer()
+                                        HStack
+                                        {
+                                            Text("Servings:" + String(A.servings)).font(.subheadline)
+                                            Text("Time:" + String(A.totalTime)).font(.subheadline)
+                                            
+                                        }
+                                        Spacer()
+                                        
                                     }
-                                    Spacer()
                                 }
-                            }
-                        }.navigationTitle("Isabel Recipe")
-
-                        
-                    }
-                    //.environment(\.defaultMinListRowHeight, 150)
+                                .padding(.bottom, 10.0)
+                                
+                                //}.navigationTitle("Isabel Recipe")
+                            }.navigationBarHidden(true)
+                            
+                        }
+                    }.padding(.leading ,10)
+                }
                 
-            
-           
+                
+                //.environment(\.defaultMinListRowHeight, 150)
+                
+                
+                
             }
+            
             Spacer()
         }
         
@@ -57,6 +69,6 @@ struct MomRecipeList: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        MomRecipeList()
+        MomRecipeList().environmentObject(MomRecipeModel())
     }
 }
